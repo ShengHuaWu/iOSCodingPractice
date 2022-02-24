82a1d8edf691cffd4e67b4e9a6650aa924c3d994
ShengHua Wu <swu1@wayfair.com>
2022-02-24 12:36:13 +0100

    Protect read/write operations with a serial queue.

--- CodingPractice/Persistence/PersistenceClient.swift
+++ CodingPractice/Persistence/PersistenceClient.swift
@@ -2,22 +2,37 @@ import Foundation
 
 final class PersistenceClient {
     private var products: [Product] = []
+    private let queue: DispatchQueue // Use a serial queue to protect reading & writing operations
+    
+    init(queue: DispatchQueue = .init(label: "PersistenceClient")) {
+        self.queue = queue
+    }
     
     func store(_ products: [Product]) {
-        self.products.append(contentsOf: products)
+        self.queue.sync { [weak self] in
+            self?.products.append(contentsOf: products)
+        }
     }
     
     func getProduct(with id: String) -> Product? {
-        return self.products.first(where: { $0.id == id })
+        return self.queue.sync { [weak self] in
+            return self?.products.first(where: { $0.id == id })
+        }
     }
     
     func toggleIsFavorited(with id: String) {
-        guard let index = self.products.firstIndex(where: { $0.id == id }) else {
-            return
+        self.queue.sync { [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            guard let index = strongSelf.products.firstIndex(where: { $0.id == id }) else {
+                return
+            }
+            
+            var product = strongSelf.products.remove(at: index)
+            product.isFavorited.toggle()
+            strongSelf.products.insert(product, at: index)
         }
-        
-        var product = self.products.remove(at: index)
-        product.isFavorited.toggle()
-        self.products.insert(product, at: index)        
     }
 }
