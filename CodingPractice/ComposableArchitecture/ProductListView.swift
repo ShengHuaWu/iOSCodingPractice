f1d573e84288034913b68cc61768b876432fe17a
ShengHua Wu <fantasy0404@gmail.com>
2022-06-10 13:24:48 +0200

    Use swift ui in router.

---
+++ CodingPractice/ComposableArchitecture/ProductListView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ProductListView: View {
+    var body: some View {
+        Text("Product List")
+    }
+}
+
+struct ProductListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductListView()
+    }
+}
--- CodingPractice/Features/Router.swift
+++ CodingPractice/Features/Router.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SwiftUI
 
 protocol Routing: AnyObject {
     func presentProductDetail(with id: String)
@@ -10,20 +11,27 @@ final class Router {
     private weak var repository: ProductFeatureRepository?
     
     func presentProducts(in window: UIWindow) {
-        let webServiceClient = WebServiceClient(
-            urlSession: .shared,
-            dataProcessor: .init(jsonDecoder: .init())
-        )
-        let repository = ProductFeatureRepository(
-            webService: webServiceClient,
-            persistence: PersistenceClient()
-        )
-        self.repository = repository
-        let viewModel = ProductsViewModel(
-            repository: repository,
-            routing: self
-        )
-        let viewController = ProductsViewController(viewModel: viewModel)
+        let viewController: UIViewController
+        if (ProcessInfo.processInfo.environment["enable_swift_ui"] != nil) {
+            let productListView = ProductListView()
+            viewController = UIHostingController(rootView: productListView)
+        } else {
+            let webServiceClient = WebServiceClient(
+                urlSession: .shared,
+                dataProcessor: .init(jsonDecoder: .init())
+            )
+            let repository = ProductFeatureRepository(
+                webService: webServiceClient,
+                persistence: PersistenceClient()
+            )
+            self.repository = repository
+            let viewModel = ProductsViewModel(
+                repository: repository,
+                routing: self
+            )
+            viewController = ProductsViewController(viewModel: viewModel)
+        }
+        
         let navigationController = UINavigationController(rootViewController: viewController)
         self.rootNavigationController = navigationController
         window.rootViewController = navigationController
