c4d5f664519cb6f609e5a9f952bd061050553716
ShengHua Wu <fantasy0404@gmail.com>
2022-06-22 20:49:08 +0200

    Append test cases.

--- CodingPracticeTests/Features/ComposableArchitecture/ComposableArchitectureTests.swift
+++ CodingPracticeTests/Features/ComposableArchitecture/ComposableArchitectureTests.swift
@@ -67,4 +67,136 @@ extension PersistenceEnvironment {
 
 final class ComposableArchitectureTests: XCTestCase {
     private let scheduler = DispatchQueue.test
+    
+    func testLoadListSuccess() {
+        let store = TestStore(
+            initialState: .init(),
+            reducer: productListReducer,
+            environment: .init(
+                webService: .success,
+                mainQueue: scheduler.eraseToAnyScheduler(),
+                persistence: .success
+            )
+        )
+        
+        store.send(.loadList)
+        scheduler.advance()
+        
+        store.receive(.listLoaded(.success(fakeProducts))) {
+            $0.rows = fakeProducts.map(ProductRowDisplayInfo.init(product:))
+        }
+    }
+    
+    func testLoadListFailure() {
+        let store = TestStore(
+            initialState: .init(),
+            reducer: productListReducer,
+            environment: .init(
+                webService: .failure,
+                mainQueue: scheduler.eraseToAnyScheduler(),
+                persistence: .success
+            )
+        )
+        
+        store.send(.loadList)
+        scheduler.advance()
+        
+        store.receive(.listLoaded(.failure(.init(webServiceError: webServiceError)))) {
+            $0.errorMessage = webServiceError.description
+        }
+    }
+    
+    func testSetNavigationSelection() {
+        let store = TestStore(
+            initialState: .init(
+                rows: fakeProducts.map(ProductRowDisplayInfo.init(product:))
+            ),
+            reducer: productListReducer,
+            environment: .init(
+                webService: .success,
+                mainQueue: scheduler.eraseToAnyScheduler(),
+                persistence: .success
+            )
+        )
+        
+        store.send(.setNavigation(fakeProduct.id)) {
+            $0.selection = .init(.init(productId: fakeProduct.id), id: fakeProduct.id)
+        }
+    }
+    
+    func testLoadDetail() {
+        let store = TestStore(
+            initialState: .init(
+                rows: fakeProducts.map(ProductRowDisplayInfo.init(product:))
+            ),
+            reducer: productListReducer,
+            environment: .init(
+                webService: .success,
+                mainQueue: scheduler.eraseToAnyScheduler(),
+                persistence: .success
+            )
+        )
+        
+        store.send(.setNavigation(fakeProduct.id)) {
+            $0.selection = .init(.init(productId: fakeProduct.id), id: fakeProduct.id)
+        }
+        
+        store.send(.detail(.loadDetail(fakeProduct.id)))
+        scheduler.advance()
+        
+        store.receive(.detail(.detailLoaded(fakeProduct))) {
+            $0.selection = .init(
+                .init(
+                    productId: fakeProduct.id,
+                    detail: .init(product: fakeProduct)
+                ),
+                id: fakeProduct.id
+            )
+        }
+    }
+    
+    func testToggleIsProductFavorited() {
+        let store = TestStore(
+            initialState: .init(
+                rows: fakeProducts.map(ProductRowDisplayInfo.init(product:))
+            ),
+            reducer: productListReducer,
+            environment: .init(
+                webService: .success,
+                mainQueue: scheduler.eraseToAnyScheduler(),
+                persistence: .success
+            )
+        )
+        
+        store.send(.setNavigation(fakeProduct.id)) {
+            $0.selection = .init(.init(productId: fakeProduct.id), id: fakeProduct.id)
+        }
+        
+        store.send(.detail(.loadDetail(fakeProduct.id)))
+        scheduler.advance()
+        
+        store.receive(.detail(.detailLoaded(fakeProduct))) {
+            $0.selection = .init(
+                .init(
+                    productId: fakeProduct.id,
+                    detail: .init(product: fakeProduct)
+                ),
+                id: fakeProduct.id
+            )
+        }
+        
+        store.send(.detail(.toggleProductIsFavorite(fakeProduct.id)))
+        scheduler.advance()
+        
+        store.receive(.detail(.detailLoaded(fakeProduct.toggleIsFavorite()))) {
+            $0.rows = [ProductRowDisplayInfo(product: fakeProduct.toggleIsFavorite())]
+            $0.selection = .init(
+                .init(
+                    productId: fakeProduct.id,
+                    detail: .init(product: fakeProduct.toggleIsFavorite())
+                ),
+                id: fakeProduct.id
+            )
+        }
+    }
 }
